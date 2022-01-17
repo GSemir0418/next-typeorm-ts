d26583aed99973c654a9750fc30fac989317d836
gsemir0418 <845217811@qq.com>
2022-01-17 10:31:10 +0800

    test manager API

--- dist/index.js
+++ dist/index.js
@@ -10,16 +10,43 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
+var _Post = require("./entity/Post");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
+    var manager, posts, p, posts2;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            console.log(connection);
+            // 在connection中取出manager
+            manager = connection.manager;
+            _context.next = 3;
+            return manager.find(_Post.Post);
+
+          case 3:
+            posts = _context.sent;
+            // 第一次查找全部Post数据应该为空
+            console.log(posts);
+            p = new _Post.Post();
+            p.title = "Post 1";
+            p.content = "我的第一篇博客"; // 保存p对象
+
+            _context.next = 10;
+            return manager.save(p);
+
+          case 10:
+            _context.next = 12;
+            return manager.find(_Post.Post);
+
+          case 12:
+            posts2 = _context.sent;
+            // 应该有我们新增的一条数据
+            console.log(posts2); // 最后要关闭连接
+
             connection.close();
 
-          case 2:
+          case 15:
           case "end":
             return _context.stop();
         }
